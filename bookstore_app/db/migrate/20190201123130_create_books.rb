ab5e0d576d75b1f5735d0f631b721a4074107d2f
root <root@webserver2018.localdomain>
2019-02-01 13:38:35 +0000

    Added book links, models and controllers

---
+++ bookstore_app/db/migrate/20190201123130_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.float :price
+      t.string :book_id
+      t.string :publisher
+      t.text :description
+      t.float :number
+
+      t.timestamps
+    end
+  end
+end
